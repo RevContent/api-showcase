7eebde5047cab7b5c41506a3a03db43ba1696c52
mike <mikeosterhout@gmail.com>
2015-04-10 16:20:36 -0400

    .htaccess to redirect all requests to index.php, backup old

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,213 +1,104 @@
 <!DOCTYPE html>
-<!--[if lte IE 6]> <html class="no-js ie  lt-ie10 lt-ie9 lt-ie8 lt-ie7 ancient oldie" lang="en-US"> <![endif]-->
-<!--[if IE 7]>     <html class="no-js ie7 lt-ie10 lt-ie9 lt-ie8 oldie" lang="en-US"> <![endif]-->
-<!--[if IE 8]>     <html class="no-js ie8 lt-ie10 lt-ie9 oldie" lang="en-US"> <![endif]-->
-<!--[if IE 9]>     <html class="no-js ie9 lt-ie10 oldie" lang="en-US"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" dir="ltr" lang="en-US"> <!--<![endif]-->
-<head>
-<meta charset="utf-8" />
-<!--[if IE]> <meta http-equiv="X-UA-Compatible" content="IE=edge"> <![endif]-->
-<title>rev:content - Native API Showcase</title>
-<!-- Set the viewport width to device width for mobile -->
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-<meta name="author" content="rev:content">
-<meta name="Description" content="rev:content - Native API Showcase" />
-<link href="css/reset.css" rel="stylesheet" type="text/css" media="screen" />
-<link href="css/contact.css" rel="stylesheet" type="text/css" media="screen" />
-<link href="css/styles.css" rel="stylesheet" type="text/css" media="screen" />
-<link href="css/jquery.fancybox.css" rel="stylesheet" type="text/css" media="screen" />
-<!--[if gt IE 8]><!--><link href="css/retina-responsive.css" rel="stylesheet" type="text/css" media="screen" /><!--<![endif]-->
-<!--[if !IE]> <link href="css/retina-responsive.css" rel="stylesheet" type="text/css" media="screen" /> <![endif]-->
-<link href="css/open-menu.css" rel="stylesheet" type="text/css" media="screen" />
-<link href="css/print.css" rel="stylesheet" type="text/css" media="print" />
-<link href="//fonts.googleapis.com/css?family=Open+Sans:400,400italic,300,300italic,600,700,800" rel="stylesheet" type="text/css" />
-<link href="//fonts.googleapis.com/css?family=Merriweather:300,400,700" rel="stylesheet" type="text/css" />
-<script src="js/modernizr.js" type="text/javascript"></script>
-</head>
-<body class="aligned">
-<!-- Preloader -->
-<div id="preloader">
-  <div id="status">
-    <div class="parent">
-      <div class="child">
-        <p class="small">loading</p>
-      </div>
-    </div>
-  </div>
-</div>
-<div id="wrap">
-  <div id="background-color"></div>
-  <header id="header"><div class="containing-wrapper">
-    <div class="logo-wrapper">
-      <h1 id="logo"><a href="index.html">rev:content</a></h1>
-      <!--<div class="tagline"><span>Native API Showcase</span></div>-->
-    </div>
-    <div id="menu-button">
-      <div class="centralizer">
-        <div class="cursor">Menu
-          <div id="nav-button"> <span class="nav-bar"></span> <span class="nav-bar"></span> <span class="nav-bar"></span> </div>
+<html ng-app="app">
+    <head>
+        <!-- Force latest IE rendering engine or ChromeFrame if installed -->
+        <!--[if IE]>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <![endif]-->
+        <base href="/">
+
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>RevContent API Showcase</title>
+
+        <link rel="shortcut icon" href="/app/resources/img/favicon.ico">
+
+        <link rel="stylesheet" href="//f.fontdeck.com/s/css/uH5+KWQnibDTJRYggGJ9XZLTAgw/api.showcase.mosterhout.dev2.dev.internal/47418.css" type="text/css" />
+
+        <script src="app/resources/vendor/jquery/dist/jquery.js"></script>
+        <script src="app/resources/vendor/angular/angular.js"></script>
+        <script src="app/resources/vendor/angular-aria/angular-aria.js"></script>
+        <script src="app/resources/vendor/angular-animate/angular-animate.js"></script>
+        <script src="app/resources/vendor/angular-ui-router/release/angular-ui-router.js"></script>
+        <script src="app/resources/vendor/ui-router-extras/release/modular/ct-ui-router-extras.core.js"></script>
+        <script src="app/resources/vendor/ui-router-extras/release/modular/ct-ui-router-extras.sticky.js"></script>
+        <script src="app/resources/vendor/angular-material/angular-material.js"></script>
+        <script src="app/resources/vendor/angular-messages/angular-messages.js"></script>
+        <script src="app/resources/vendor/imagesloaded/imagesloaded.pkgd.js"></script>
+        <script src="app/resources/vendor/bootpag/lib/jquery.bootpag.js"></script>
+        <script src="app/resources/vendor/lodash/dist/lodash.js"></script>
+
+        <!-- plugin js -->
+        <script src="showcase/revcontent-api-showcase-grid-1.0/js/grid.js"></script>
+        <script src="showcase/revcontent-api-showcase-carousel-1.0/js/shoveler.js"></script>
+        <!-- toaster no plugin js -->
+
+        <!-- app js -->
+        <script src="app/resources/js/app/app.module.js"></script>
+        <script src="app/resources/js/app/config/app.config.js"></script>
+        <script src="app/resources/js/app/config/app.routes.js"></script>
+        <script src="app/resources/js/app/app.filters.js"></script>
+        <script src="app/resources/js/app/app.directives.js"></script>
+        <script src="app/resources/js/app/grid/grid.controller.js"></script>
+
+        <!-- plugin css -->
+        <link rel="stylesheet" type="text/css" href="showcase/revcontent-api-showcase-grid-1.0/css/grid.css">
+        <link rel="stylesheet" type="text/css" href="showcase/revcontent-api-showcase-carousel-1.0/css/carousel.css">
+        <link rel="stylesheet" type="text/css" href="showcase/revcontent-api-showcase-toaster-1.0/css/toaster.css">
+
+        <link href="app/resources/vendor/sanitize-css/dist/sanitize.css" rel="stylesheet" type="text/css" media="screen" />
+        <link href="app/resources/vendor/angular-material/angular-material.css" rel="stylesheet" type="text/css" media="screen" />
+        <link href="app/resources/css/app.css" rel="stylesheet" type="text/css" media="screen" />
+
+    </head>
+    <body>
+
+        <div id="wrapper">
+            <header>
+                <md-toolbar class="md-primary">
+                    <div class="md-toolbar-tools container md-toolbar-tools-bottom" flex="" tabindex="0">
+                        <div layout="row" flex="">
+
+                            <div flex style="position:absolute;">
+                                <h1>RevContent API Showcase</h1>
+                                <a class="logo" href="/">
+                                    <img src="/app/resources/img/revcontent-logo-dark.png">
+                                </a>
+                            </div>
+
+                            <div flex=""></div>
+
+                            <a class="md-button" target="_self" ng-href="" tabindex="0" style="margin-right:8px;">
+                                <span>Docs</span>
+                                <md-tooltip>Read Up!</md-tooltip>
+                            </a>
+
+                            <a class="md-button" target="_self" ng-href="" tabindex="0">
+                                <span>Support</span>
+                                <md-tooltip>How can we help?</md-tooltip>
+                            </a>
+
+                        </div>
+                    </div>
+                </md-toolbar>
+            </header>
+
+            <main class="container">
+                <div layout layout-padding>
+                    <md-whiteframe class="md-whiteframe-z1 banner">
+                        <div class="md-banner">Hi, We’re <strong>rev:content</strong>. A native network making brands live up to their full potential. That’s it. And this is our API showcase.</div>
+                    </md-whiteframe>
+                </div>
+
+                <div ui-view="main"></div>
+            </main>
+
+            <footer>
+                <div class="container">
+                    © 2015, rev:content All Rights Reserved. <span class="padding">&middot;</span> <a href="404.html" title=""><strong>Legal Notice</strong></a>
+                </div>
+            </footer>
         </div>
-      </div>
-    </div></div>
-  </header>
-  <!-- end header -->
-  <!-- start main nav --><div class="containing-wrapper menu">
-  <nav id="main-nav">
-    <div id="menu-close-button">&times;</div>
-    <ul id="options" class="option-set clearfix" data-option-key="filter">
-      <li class="selected"> <a href="#home">API Home</a> </li>
-      <li> <a href="#portfolio" class="sub-nav-toggle">API Examples</a>
-        <ul class="sub-nav hidden">
-          <li> <a href="#illustration">Illustration</a> </li>
-          <li> <a href="#photography">Photography</a> </li>
-          <li> <a href="#webdesign">Webdesign</a> </li>
-          <li> <a href="#portfolio">Show All</a> </li>
-        </ul>
-      </li>
-      <li> <a href="#services">API Documentation</a> </li>
-      <li> <a href="#about">API SUPPORT</a> </li>
-      <!--<li> <a href="#pricing">Pricing</a> </li>
-      <li> <a href="#blog">Blog</a> </li>
-      <li> <a href="#contact">Contact</a> </li>-->
-    </ul>
-    <div class="additional-links">
-      <ul>
-        <li> <a href="project.html">Project Detail</a> </li>
-        <li> <a href="icons.html">Icons</a> </li>
-        <li> <a href="slider.html">Slideshow</a> </li>
-        <li> <a href="video.html">Vimeo</a> </li>
-        <li> <a href="youtube.html">Youtube</a> </li>
-        <li> <a href="post.html">Post Page</a> </li>
-      </ul>
-    </div>
-    <div class="social-links">
-      <ul class="social-list clearfix">
-        <li> <a href="#" class="pinterest"></a> </li>
-        <li> <a href="#" class="twitter"></a> </li>
-        <li> <a href="#" class="gplus"></a> </li>
-        <li> <a href="#" class="facebook"></a> </li>
-      </ul>
-    </div>
-  </nav></div>
-  <!-- end main nav -->
-  <div class="content-wrapper">
-    <div id="content">
-      <div class="container">
-        <div id="container" class="clearfix">
-
-          <div class="element  clearfix col2-3 home about grey grid-fit">
-            <p class="small">Native API Showcase</p>
-            <h2>Hi, We’re rev:content. <br />
-              A <strong>native network</strong> making brands live up to their full potential. That’s it. And this is our API showcase.</h2>
-          </div>
-
-          <div class="element  clearfix col1-3 home portfolio photography"> <a href="project.php?id=endofgallery" title="End Of Gallery">
-            <figure class="images"> <img src="images/endgallery.png" alt="<span>End Of Gallery</span><i>→</i>" class="slip" /> </figure>
-            </a>
-          </div>
-
-          <div class="element  clearfix col1-3 home portfolio webdesign"> <a href="project.php?id=toaster" title="Content Toaster">
-            <figure class="images"> <img src="images/toaster.png" alt="<span>Content Toaster</span><i>→</i>" class="slip" /> </figure>
-            </a>
-          </div>
-
-          <div class="element  clearfix col1-3 home portfolio"> <a href="project.php?id=instream" title="">
-            <figure class="images"> <img src="images/homeloop.png" alt="<span>Instream</span><i>→</i>" class="slip" /> </figure>
-            </a>
-          </div>
-
-          <div class="element  clearfix col1-3 home grey"> <a href="https://v2.revcontent.com/signup" data-title="Sign Up" >
-            <p class="small">don't have an account?</p>
-            <h3>Join the native rev:olution</h3>
-            <div class="bottom">
-              <div class="icons document"></div>
-              <p class="alignleft">Signup For Free</p>
-              <span class="arrow">→</span></div>
-            </a>
-          </div>
-
-          <div class="element  clearfix col1-3 portfolio photography home"> <a href="project.html" title="">
-            <figure class="images"> <img src="images/parallax.png" alt="<span>Parallax Photo-text</span><i>→</i>" class="slip" /> </figure>
-            </a>
-          </div>
-
-
-          <div class="element  clearfix col1-3 services grey auto">
-            <div class="icons search"></div>
-            <h3>Search Engine Optimization (SEO)</h3>
-            <ul class="unordered-list">
-              <li>A supercool hipster collection</li>
-              <li>Came across the table</li>
-              <li>This is just dummy text in an unordered list</li>
-            </ul>
-          </div>
-
-          <div class="element  clearfix col1-3 portfolio photography home"> <a href="project.php?id=carousel" title="">
-            <figure class="images"> <img src="images/carousel.png" alt="<span>Carousel</span><i>→</i>" class="slip" /> </figure>
-            </a> </div>
-
-          <div class="element  clearfix col1-3 portfolio photography home"> <a href="project.php?id=grid" title="">
-            <figure class="images"> <img src="images/carousel.png" alt="<span>Grid</span><i>→</i>" class="slip" /> </figure>
-            </a> </div>
-
-          <div class="element  clearfix col1-3 home grey"> <a href="http://www.github.com/RevContent" data-title="" class="whole-tile">
-            <p class="small">GitHub</p>
-            <h3>All of our awesome API examples are open source</h3>
-            <div class="bottom">
-              <div class="icons like"></div>
-              <p class="alignleft">Checkout the Code</p>
-              <span class="arrow">→</span></div>
-            </a>
-          </div>
-
-          <div class="element  clearfix col1-3 portfolio photography home"> <a href="project.php?id=scrolling" title="Scrolling Experience">
-            <figure class="images"> <img src="images/scrolling.png" alt="<span>Scrolling Experience</span><i>→</i>" class="slip" /> </figure>
-            </a>
-          </div>
-
-          <div class="element  clearfix col1-3 portfolio illustration"> <a href="project.html" title="">
-            <figure class="images"> <img src="images/work10.jpg" alt="<span>Book Cover</span><i>→</i>" class="slip" /> </figure>
-            </a>
-          </div>
-
-
-        </div>
-        <!-- end #container -->
-      </div>
-      <!-- end .container -->
-    </div>
-    <!-- end content -->
-  </div>
-  <!-- end content-wrapper -->
-</div>
-<!-- end wrap -->
-<footer id="footer" class="clearfix"><div class="containing-wrapper">
-  <p class="alignleft">© 2015, rev:content All Rights Reserved. <span class="padding">&middot;</span> <a href="404.html" title=""><strong>Legal Notice</strong></a></p>
-  <p class="alignright"><a href="">Sign Up</a> <span class="padding">&middot;</span> <a href="mailto:support@revcontent.com" title="Need Help?">support@revcontent.com</a> </p>
-
-  <ul class="social-list clearfix">
-    <li> <a href="#" class="pinterest"></a> </li>
-    <li> <a href="#" class="twitter"></a> </li>
-    <li> <a href="#" class="gplus"></a> </li>
-    <li> <a href="#" class="facebook"></a> </li>
-  </ul>
-
-
-  </div>
-</footer>
-<script src="js/jquery-1.11.1.min.js" type="text/javascript"></script>
-<script src="js/jquery-easing-1.3.js" type="text/javascript"></script>
-<script src="js/retina.js" type="text/javascript"></script>
-<script src="js/jquery.touchSwipe.min.js" type="text/javascript"></script>
-<script src="js/jquery.isotope2.min.js" type="text/javascript"></script>
-<script src="js/jquery.ba-bbq.min.js" type="text/javascript"></script>
-<script src="js/jquery.isotope.load.js" type="text/javascript"></script>
-<script src="js/SmoothScroll.js" type="text/javascript"></script>
-<script src="js/main.js" type="text/javascript"></script>
-<script src="js/jquery.form.js" type="text/javascript"></script>
-<script src="js/input.fields.js" type="text/javascript"></script>
-<script src="js/preloader.js" type="text/javascript"></script>
-<script src="js/jquery.fancybox.pack.js" type="text/javascript"></script>
-<script src="js/jquery.sliphover.min.js"></script>
-</body>
+    </body>
 </html>
