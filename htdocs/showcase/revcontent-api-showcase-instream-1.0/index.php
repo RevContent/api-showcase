5bcbe5931005384434c1c418c2b71286d621c3b1
Michael Osterhout <michael@revcontent.com>
2015-05-04 15:58:32 -0400

    sync showcase examples with public git repo

--- htdocs/showcase/revcontent-api-showcase-instream-1.0/index.php
+++ htdocs/showcase/revcontent-api-showcase-instream-1.0/index.php
@@ -2,35 +2,21 @@
 /**
 * Example API call in PHP for the RevContent API
 *
-* Got Questions? Email Us: support@RevContent.com
+* Got Questions? Email Us: support@revcontent.com
 *
 */
     error_reporting(-1);
     ini_set('display_errors', 'on');
     ini_set('html_errors', 'on');
 
-    $revcontentAPI = 'https://trends.revcontent.com/api/v1/?';
-    $api_key       = 'a3ef3561e4a67bc7ef7e0aff5a4889d19007c957';
-    $pub_id        = 307;
-    $widget_id     = 67;
-    $domain        = "247sports.com";
-    $count         = 2;
-    $offset        = 0;
-    $format        = "json";
-
-
-    $url = $revcontentAPI . http_build_query(array(
-        'api_key' => $api_key,
-        'pub_id' => $pub_id,
-        'widget_id' => $widget_id,
-        'domain' => $domain,
-        'count' => $count,
-        'offset' => $offset,
-        'format' => $format
+    $url = 'https://trends.revcontent.com/api/v1/?' . http_build_query(array(
+        'api_key' => 'fdbd849fb2b8c1c259585e7bcd6a09cc87e3e5b7',
+        'pub_id' => 10,
+        'widget_id' => 21,
+        'domain' => 'powr.com',
+        'count' => 2,
+        'offset' => 0,
     ));
-
-
-
     // Make the API call
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
@@ -40,9 +26,6 @@
 
     // Decode and print the response
     $response = json_decode($output, true);
-
-    //var_dump($response);
-   //var_dump($url);
 
 ?>
 
@@ -63,14 +46,11 @@
 
     <title>In-stream Example</title>
 
-    <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap.css" rel="stylesheet">
 
-    <!-- Showcase CSS -->
     <link href="css/style.css" rel="stylesheet">
 
-    <!-- Custom CSS -->
-    <link href="css/homepageloop.css" rel="stylesheet">
+    <link href="css/example.css" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
--- htdocs/showcase/revcontent-api-showcase-scrollingexp-1.0/README.md
+++ htdocs/showcase/revcontent-api-showcase-scrollingexp-1.0/README.md
@@ -1,26 +1,3 @@
-# [Start Bootstrap](http://startbootstrap.com/) - [Bare](http://startbootstrap.com/template-overviews/bare/)
+# Scrolling Experience
 
-[Bare](http://startbootstrap.com/template-overviews/bare/) is a bare HTML starter template for [Bootstrap](http://getbootstrap.com/) created by [Start Bootstrap](http://startbootstrap.com/). This template features predefined file paths for Bootstrap and jQuery, so you can quickly create Bootstrap websites.
-
-## Getting Started
-
-To use this template, choose one of the following options to get started:
-* Download the latest release on Start Bootstrap
-* Fork this repository on GitHub
-
-## Bugs and Issues
-
-Have a bug or an issue with this template? [Open a new issue](https://github.com/IronSummitMedia/startbootstrap-bare/issues) here on GitHub or leave a comment on the [template overview page at Start Bootstrap](http://startbootstrap.com/template-overviews/bare/).
-
-## Creator
-
-Start Bootstrap was created by and is maintained by **David Miller**, Managing Parter at [Iron Summit Media Strategies](http://www.ironsummitmedia.com/).
-
-* https://twitter.com/davidmillerskt
-* https://github.com/davidtmiller
-
-Start Bootstrap is based on the [Bootstrap](http://getbootstrap.com/) framework created by [Mark Otto](https://twitter.com/mdo) and [Jacob Thorton](https://twitter.com/fat).
-
-## Copyright and License
-
-Copyright 2013-2015 Iron Summit Media Strategies, LLC. Code released under the [Apache 2.0](https://github.com/IronSummitMedia/startbootstrap-bare/blob/gh-pages/LICENSE) license.
+AJAX-powered infinite scrolling UI’s are everywhere these days, with our API you can easily integrate sponsored content into your own implementation. Utilizing our count and offset request parameters to fuel the implementation, you can create a truly native experience that will engage user’s as they scroll.
