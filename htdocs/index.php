7a7418c4eb5e2a1f13c7ec7adde8006beb6fb097
Michael Osterhout <michael@revcontent.com>
2015-09-16 12:18:40 -0400

    new logo

--- htdocs/index.php
+++ htdocs/index.php
@@ -56,10 +56,8 @@
                             <div>
                                 <div class="navbar-header">
                                     <div class="navbar-brand">
-                                        <a href="/" class="navbar-logo" title="Revcontent.com">
-                                            <span class="icon-rc-logo-dark logo-normal">
-                                                <span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span><span class="path6"></span><span class="path7"></span><span class="path8"></span><span class="path9"></span><span class="path10"></span><span class="path11"></span><span class="path12"></span><span class="path13"></span><span class="path14"></span><span class="path15"></span><span class="path16"></span><span class="path17"></span><span class="path18"></span><span class="path19"></span><span class="path20"></span><span class="path21"></span><span class="path22"></span><span class="path23"></span><span class="path24"></span><span class="path25"></span><span class="path26"></span><span class="path27"></span><span class="path28"></span><span class="path29"></span><span class="path30"></span><span class="path31"></span><span class="path32"></span>
-                                            </span>
+                                        <a href="/" class="navbar-logo" title="Revcontent Innovation Lab">
+                                            <img src="app/resources/img/revcontent-labs.png" alt="Revcontent Innovation Lab">
                                         </a>
                                     </div>
                                 </div>
