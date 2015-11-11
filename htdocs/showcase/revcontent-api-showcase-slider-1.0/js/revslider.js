820bd49401645d7651cbb421636a71663c3629f8
Michael Osterhout <michael@revcontent.com>
2015-11-11 14:47:10 -0500

    WIP refactor - working without pagination

--- htdocs/showcase/revcontent-api-showcase-slider-1.0/js/revslider.js
+++ htdocs/showcase/revcontent-api-showcase-slider-1.0/js/revslider.js
@@ -20,7 +20,16 @@ RevSlider({
         mobile: false,
         desktop: true
     },
-    perRow: {
+    rows: {
+        xxs: 2,
+        xs: 2,
+        sm: 2,
+        md: 2,
+        lg: 2,
+        xl: 2,
+        xxl: 2
+    },
+    per_row: {
         xxs: 1,
         xs: 1,
         sm: 3,
@@ -31,12 +40,12 @@ RevSlider({
     },
     image_ratio: (revDetect.mobile() ? 'wide_rectangle' : 'rectangle'),
     rev_position: (revDetect.mobile() ? 'bottom_right' : 'top_right'),
-    internal: false,
     header: 'Trending Now',
     devices: [
         'phone', 'tablet', 'desktop'
     ],
-    url: 'https://trends.revcontent.com/api/v1/'
+    url: 'https://trends.revcontent.com/api/v1/',
+    ad_border: true
 });
 */
 
@@ -53,6 +62,15 @@ RevSlider({
 
         var defaults = {
             element: false,
+            rows: {
+                xxs: 2,
+                xs: 2,
+                sm: 2,
+                md: 2,
+                lg: 2,
+                xl: 2,
+                xxl: 2
+            },
             per_row: {
                 xxs: 1,
                 xs: 1,
@@ -74,7 +92,8 @@ RevSlider({
             devices: [
                 'phone', 'tablet', 'desktop'
             ],
-            url: 'https://trends.revcontent.com/api/v1/'
+            url: 'https://trends.revcontent.com/api/v1/',
+            ad_border: true
         };
 
         // merge options
@@ -95,15 +114,20 @@ RevSlider({
 
         var backBtn = document.createElement('div');
         backBtn.id = "back";
-        backBtn.innerHTML = '<button id="btn-back" class="btn btn-primary"><</button>';
+        backBtn.setAttribute('class', 'rev-btn-container');
+        backBtn.innerHTML = '<button id="btn-back" class="rev-btn"><</button>';
 
         var forwardBtn = document.createElement('div');
         forwardBtn.id = "forward";
-        forwardBtn.innerHTML = '<button id="btn-forward" class="btn btn-primary">></button>';
+        forwardBtn.setAttribute('class', 'rev-btn-container');
+        forwardBtn.innerHTML = '<button id="btn-forward" class="rev-btn">></button>';
 
         var containerElement = document.createElement('div');
         containerElement.id = 'rev-slider';
         containerElement.class = 'rev-slider';
+
+        var gridContainerElement = document.createElement('div');
+        gridContainerElement.id = 'rev-slider-grid-container';
 
         var gridElement = document.createElement('div');
         gridElement.id = 'rev-slider-grid';
@@ -111,15 +135,21 @@ RevSlider({
         var element = this.options.element ? this.options.element[0] : document.getElementById(this.options.id);
         element.style.width = '100%';
 
-        revUtils.append(containerElement, backBtn);
-        revUtils.append(containerElement, gridElement);
-        revUtils.append(containerElement, forwardBtn);
+        revUtils.append(containerElement, gridContainerElement);
+        revUtils.append(gridContainerElement, backBtn);
+        revUtils.append(gridContainerElement, gridElement);
+        revUtils.append(gridContainerElement, forwardBtn);
         revUtils.append(element, containerElement);
 
-        var grid = new AnyGrid(gridElement, { masonry: false, perRow: this.options.per_row });
+        var grid = new AnyGrid(gridElement, { masonry: false, perRow: this.options.per_row, transitionDuration: 0});
+
+        var getLimit = function() {
+            // can pass object for rows or just single value for all breakpoints
+            return grid.getPerRow() * (that.options.rows[grid.getBreakPoint()] ? that.options.rows[grid.getBreakPoint()] : that.options.rows);
+        }
 
         var page = 1,
-            limit = grid.getPerRow() * 2,
+            limit = getLimit(),
             resizeTimeout;
 
         grid.on('resized', function() {
@@ -127,71 +157,175 @@ RevSlider({
                 clearTimeout(resizeTimeout);
             }
             function delay() {
-                var newLimit = grid.getPerRow() * 2;
+                var newLimit = getLimit();
+                var reconfig = 0;
                 if (newLimit != limit) {
+                    reconfig = (newLimit - limit);
                     limit = newLimit;
-                    setData();
+                    getData();
                 }
+                resize(reconfig);
                 resizeTimeout = false;
             }
             resizeTimeout = setTimeout(delay, 300);
         });
 
-        var fade = function(fade) {
-            var children = gridElement.children;
-            for (var i = 0; i < children.length; i++) {
-              var child = children[i];
-              child.classList.add(fade);
+        if (this.options.image_ratio == 'square') {
+            var imageHeight = 400;
+            var imageWidth = 400;
+        } else if (this.options.image_ratio == 'rectangle') {
+            var imageHeight = 300;
+            var imageWidth = 400;
+        } else if (this.options.image_ratio == 'wide_rectangle') {
+            var imageHeight = 450;
+            var imageWidth = 800;
+        }
+
+        var appendElements = function() {
+            var header = document.createElement('h2');
+            header.innerHTML = that.options.header;
+            revUtils.addClass(header, 'rev-header');
+            revUtils.prepend(containerElement, header);
+
+            var sponsored = document.createElement('div');
+            revUtils.addClass(sponsored, 'rev-sponsored');
+            sponsored.innerHTML = '<a href="http://revcontent.com" target="_blank">Sponsored by Revcontent</a>';
+            if (that.options.rev_position == 'top_right') {
+                revUtils.addClass(sponsored, 'top-right')
+                revUtils.prepend(containerElement, sponsored);
+            } else if (that.options.rev_position == 'bottom_left' || that.options.rev_position == 'bottom_right') {
+                revUtils.addClass(sponsored, that.options.rev_position.replace('_', '-'));
+                revUtils.append(containerElement, sponsored);
             }
         }
 
-        var setData = function() {
-            var minDelay = (gridElement.children.length > 0) ? 300 : 0;
-            var start = new Date();
-            var url = that.options.url + '/?api_key=3eeb00d786e9a77bbd630595ae0be7e9aa7aff3b&pub_id=945&widget_id=6181&domain=apiexamples.powr.com&sponsored_count=' + limit + '&sponsored_offset=' + ((page * limit) - limit) + '&internal_count=0';
+        appendElements();
 
-            fade('fadeOut');
+        var setUp = function() {
+            var width = grid.containerWidth / grid.perRow;
 
-            var respond = function(resp) {
-                var loader = document.createElement('div');
-                var containerHtml = '';
-                for (var i in resp) {
-                    var node = document.createElement('div');
-                    var html = '<div style="opacity:0"><a href="'+ resp[i].url +'"><img class="img-responsive" src="'+ resp[i].image +'"/><h3>'+ resp[i].headline +'</h3></a></div>';
-                    node.innerHTML = html;
-                    containerHtml += html;
-                    loader.appendChild(node);
-                }
-                imagesLoaded( loader, function() {
-                    gridElement.innerHTML = containerHtml;
-                    fade('fadeIn');
+            that.padding = ((width * .025).toFixed(2) / 1);
+            that.innerMargin = ((width * .02).toFixed(2) / 1);
 
-                    grid.reloadItems();
-                    grid.layout();
-                });
-            }
+            // // font size is relative to width, other measurements are relative to this font size
+            that.headlineFontSize = Math.max(14, ((width * .03).toFixed(2) / 1));
+            that.headlineLineHeight = ((that.headlineFontSize * 1.25).toFixed(2) / 1);
+            that.headlineHeight = ((that.headlineLineHeight * 2).toFixed(2) / 1);
+            that.headlineMarginTop = ((that.headlineHeight * .2).toFixed(2) / 1);
+
+            that.providerFontSize = Math.max(11, ((that.headlineLineHeight / 2).toFixed(2) / 1));
+            that.providerLineHeight = ((that.providerFontSize * 1.25).toFixed(2) / 1);
+            that.providerMargin = ((that.providerLineHeight * .2).toFixed(2) / 1);
+
+            that.preloaderHeight = (grid.columnWidth - (that.padding * 2) - (that.options.ad_border ? 2 : 0)) * (imageHeight / imageWidth);
+        }
+
+        setUp();
+
+        var appendCell = function() {
+            var html = '<div class="rev-ad" style="'+ (that.options.ad_border ? 'border:1px solid #eee' : '') +'">' +
+                        '<a href="" target="_blank">' +
+                            '<div class="rev-image" style="height:'+ that.preloaderHeight +'px">' +
+                                '<img src=""/>' +
+                            '</div>' +
+                            '<div class="rev-headline" style="height:'+ that.headlineHeight +'px; margin:'+ that.headlineMarginTop +'px ' + that.innerMargin + 'px' + ' 0;">' +
+                                '<h3 style="font-size:'+ that.headlineFontSize +'px; line-height:'+ that.headlineLineHeight +'px;"></h3>' +
+                            '</div>' +
+                            '<div style="margin:' + that.providerMargin +'px '  + that.innerMargin + 'px ' + that.providerMargin +'px;font-size:'+ that.providerFontSize +'px;line-height:'+ that.providerLineHeight +'px;height:'+ that.providerLineHeight +'px;" class="rev-provider"></div>' +
+                        '</a>' +
+                    '</div>';
+            var cell = document.createElement('div');
+
+            cell.style.padding = that.padding + 'px';
+
+            revUtils.addClass(cell, 'rev-content');
+
+            cell.innerHTML = html;
+
+            gridElement.appendChild(cell);
+        };
+
+        for (var i = 0; i < limit; i++) {
+            appendCell();
+        };
+
+        grid.reloadItems();
+        grid.layout();
+
+        var getData = function() {
+            var url = that.options.url + '?api_key='+ that.options.api_key +'&pub_id='+ that.options.pub_id +'&widget_id='+ that.options.widget_id +'&domain='+ that.options.domain +'&sponsored_count=' + limit + '&sponsored_offset=' + ((page * limit) - limit) + '&internal_count=0';
 
             revApi.request(url, function(resp) {
-                var end = new Date();
-                var requestTime = end - start;
-                if (requestTime  < minDelay) {
-                    setTimeout(function() { respond(resp); }, minDelay - requestTime );
-                } else {
-                    respond(resp);
+
+                var ads = element.querySelectorAll('.rev-ad');
+
+                for (var i = 0; i < resp.length; i++) {
+                    var ad = ads[i],
+                        data = resp[i];
+                    ad.querySelectorAll('a')[0].setAttribute('href', data.url);
+                    ad.querySelectorAll('img')[0].setAttribute('src', data.image);
+                    ad.querySelectorAll('.rev-headline h3')[0].innerHTML = data.headline;
+                    ad.querySelectorAll('.rev-provider')[0].innerHTML = data.brand;
                 }
+
+                imagesLoaded( gridElement, function() {
+                    revUtils.addClass(containerElement, 'loaded');
+                });
             });
         };
 
-        setData();
+        getData();
+
+
+        var resize = function(reconfig) {
+            setUp();
+            if (reconfig !== 0) {
+                var nodes = element.querySelectorAll('.rev-content');
+                if (reconfig < 0) {
+                    for (var i = 0; i < nodes.length; i++) {
+                        if (i >= limit) {
+                            grid.remove(nodes[i]);
+                        }
+                    };
+                } else {
+                    var add = limit - nodes.length;
+                    for (var i = 0; i < add; i++) {
+                        appendCell();
+                    }
+                }
+            }
+
+
+            var ads = element.querySelectorAll('.rev-content');
+
+            for (var i = 0; i < ads.length; i++) {
+                var ad = ads[i];
+
+                ad.style.padding = that.padding + 'px';
+
+                ad.querySelectorAll('.rev-image')[0].style.height = that.preloaderHeight + 'px';
+                ad.querySelectorAll('.rev-headline')[0].style.height = that.headlineHeight + 'px';
+                ad.querySelectorAll('.rev-headline')[0].style.margin = that.headlineMarginTop +'px ' + that.innerMargin + 'px 0';
+
+                ad.querySelectorAll('.rev-headline h3')[0].style.fontSize = that.headlineFontSize +'px';
+                ad.querySelectorAll('.rev-headline h3')[0].style.lineHeight = that.headlineLineHeight +'px';
+                ad.querySelectorAll('.rev-provider')[0].style.margin = that.providerMargin +'px '  + that.innerMargin + 'px ' + that.providerMargin +'px';
+                ad.querySelectorAll('.rev-provider')[0].style.fontSize = that.providerFontSize +'px';
+                ad.querySelectorAll('.rev-provider')[0].style.lineHeight = that.providerLineHeight + 'px';
+                ad.querySelectorAll('.rev-provider')[0].style.height = that.providerLineHeight +'px';
+            }
+            grid.reloadItems();
+            grid.layout();
+        };
 
         document.getElementById('btn-forward').addEventListener('click', function() {
             page = page + 1;
-            setData();
+            getData();
         });
 
         document.getElementById('btn-back').addEventListener('click', function() {
             page = Math.max(1, page - 1);
-            setData();
+            getData();
         });
     };
 
