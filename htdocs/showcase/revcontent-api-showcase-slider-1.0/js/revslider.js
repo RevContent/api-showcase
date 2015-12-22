56e21b9008665ae191ed3c38c6ed86b261158ef2
Michael Osterhout <michael@revcontent.com>
2015-12-22 15:43:58 -0500

    default masonry and stacked to false, make is_layout_instant and transition_duration options

--- htdocs/showcase/revcontent-api-showcase-slider-1.0/js/revslider.js
+++ htdocs/showcase/revcontent-api-showcase-slider-1.0/js/revslider.js
@@ -62,6 +62,8 @@ RevSlider({
 
         var defaults = {
             element: false,
+            masonry: false,
+            stacked: false,
             rows: {
                 xxs: 2,
                 xs: 2,
@@ -96,7 +98,9 @@ RevSlider({
             ad_border: true,
             headline_size: 2,
             max_headline: false,
-            text_overlay: false
+            text_overlay: false,
+            is_layout_instant: false,
+            transition_duration: 0
         };
 
         // merge options
@@ -144,7 +148,14 @@ RevSlider({
         // revUtils.append(gridContainerElement, forwardBtn);
         revUtils.append(this.element, this.containerElement);
 
-        this.grid = new AnyGrid(this.gridElement, { masonry: false, perRow: this.options.per_row, transitionDuration: 0, isResizeBound: this.options.is_resize_bound});
+        this.grid = new AnyGrid(this.gridElement, {
+            masonry: this.options.masonry,
+            stacked: this.options.stacked,
+            perRow: this.options.per_row,
+            isLayoutInstant: this.options.is_layout_instant,
+            transitionDuration: this.options.transition_duration,
+            isResizeBound: this.options.is_resize_bound
+        });
 
         this.grid.on('resized', function() {
             that.resize();
