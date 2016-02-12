3f0f58a2cd1345464efc9941982cf0dbc1bbb14d
Michael Osterhout <michael@revcontent.com>
2016-02-12 15:09:43 -0500

    add options

--- htdocs/showcase/revcontent-api-showcase-lock-1.0/js/revlock.js
+++ htdocs/showcase/revcontent-api-showcase-lock-1.0/js/revlock.js
@@ -31,9 +31,14 @@ RevLock({
     var RevLock = function(opts) {
 
         var defaults = {
+            id: false,
+            distance: 500,
             element: false,
+            unlock_text: 'Read More...',
             inner_widget_options: {
                 header: 'Trending Now',
+                rev_position: 'top_right',
+                image_ratio: 'rectangle',
                 per_row: {
                     xxs: 2,
                     xs: 2,
@@ -43,21 +48,23 @@ RevLock({
                     xl: 6,
                     xxl: 7
                 },
+                buttons: {
+                    forward: true,
+                    back: false,
+                    size: 40,
+                    position: 'outside',
+                },
                 rows: 2
             },
             devices: [
                 'phone', 'tablet', 'desktop'
-            ],
-            buttons: {
-                forward: true,
-                back: false,
-                size: 40,
-                position: 'outside',
-            }
+            ]
         };
 
         // merge options
         this.options = revUtils.extend(defaults, opts);
+
+        this.options.inner_widget_options = revUtils.extend(defaults.inner_widget_options, opts.inner_widget_options);
 
         if (revUtils.validateApiParams(this.options).length) {
             return;
@@ -88,16 +95,19 @@ RevLock({
 
             this.unlockBtn = document.createElement('div');
             this.unlockBtn.id = 'rev-lock-unlock';
-            this.unlockBtn.innerHTML = 'Unlock';
+            this.unlockBtn.innerHTML = this.options.unlock_text;
 
             this.containerElement = document.createElement('div');
             this.containerElement.className = 'rev-lock-inner';
 
             this.innerWidgetElement = document.createElement('div');
 
-            this.marker = document.getElementById('check').getBoundingClientRect();
-            this.top = this.marker.top + document.body.scrollTop;
-            this.element.style.top = this.top + 'px';
+            var marker = document.getElementById(this.options.id);
+            var top = marker ? marker.getBoundingClientRect().top : this.options.distance;
+
+            top = top + document.body.scrollTop;
+
+            this.element.style.top = top + 'px';
 
             revUtils.append(this.containerElement, this.innerWidgetElement);
             revUtils.append(this.element, this.unlockBtn);
@@ -106,21 +116,22 @@ RevLock({
             revUtils.append(document.body, this.element);
 
             this.innerWidget = new RevSlider({
-                element: [this.innerWidgetElement],
-                url: 'https://trends.revcontent.com/api/v1/',
-                api_key : 'bf3f270aa50d127f0f8b8c92a979d76aa1391d38',
-                pub_id : 7846,
-                widget_id : 13523,
-                domain : 'bustle.com',
-                rev_position: 'top_right',
-                per_row: this.options.inner_widget_options.per_row,
-                rows: this.options.inner_widget_options.rows,
-                image_ratio: 'rectangle',
-                vertical: true,
-                buttons: this.options.buttons
+                element:      [this.innerWidgetElement],
+                url:          'https://trends.revcontent.com/api/v1/',
+                api_key:      'bf3f270aa50d127f0f8b8c92a979d76aa1391d38',
+                pub_id:       7846,
+                widget_id:    13523,
+                domain:       'bustle.com',
+                rev_position: this.options.inner_widget_options.rev_position,
+                header:       this.options.inner_widget_options.header,
+                per_row:      this.options.inner_widget_options.per_row,
+                rows:         this.options.inner_widget_options.rows,
+                image_ratio:  this.options.inner_widget_options.image_ratio,
+                buttons:      this.options.inner_widget_options.buttons,
+                vertical:     true,
             });
 
-            this.totalHeight = this.top + this.element.offsetHeight + 'px';
+            this.totalHeight = top + this.element.offsetHeight + 'px';
 
             this.wrapper.style.height = this.totalHeight;
 
