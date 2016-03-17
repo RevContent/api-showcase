ca58421efc3196385c746197817390c2ac01efc7
Michael Osterhout <michael@revcontent.com>
2016-03-17 12:56:48 -0400

    build

--- htdocs/showcase/revcontent-api-showcase-slider-1.0/build/revslider.pkgd.js
+++ htdocs/showcase/revcontent-api-showcase-slider-1.0/build/revslider.pkgd.js
@@ -5045,7 +5045,6 @@ return utils;
 
 (function (window, document, dialog, undefined) {
     'use strict';
-    // console.log("Entering RevDisclose Namespace...");
     var RevDisclose = function () {
         var self = this;
         self.dialog = dialog;
@@ -5064,18 +5063,15 @@ return utils;
     };
 
     RevDisclose.prototype.init = function () {
-        // console.log("RevDisclose: Initializing...");
         var self = this;
         document.onreadystatechange = function () {
             if (document.readyState == "complete") {
-                // console.log("RevDisclose: Document is READY!...");
 
             }
         }
     };
 
     RevDisclose.prototype.setDialog = function(dialog){
-        // console.log("RevDisclose: Manually Setting Dialog Object");
         var self = this;
         if(typeof dialog === "object"){
             self.dialog = dialog;
@@ -5083,20 +5079,17 @@ return utils;
     };
 
     RevDisclose.prototype.truncateDisclosure = function () {
-        // console.log("RevDisclose: Initializing...");
         var self = this;
         self.disclosureText = self.disclosureText.toString().substring(0, self.disclosureTextLimit).replace(/['"]+/g, '');
     };
 
     RevDisclose.prototype.setDisclosureText = function(disclosure){
-        // console.log("RevDisclose: Setting Disclosure Text Label to: "  + disclosure.toString());
         var self = this;
         self.disclosureText = (disclosure.length > 2) ? disclosure.toString() : self.defaultDisclosureText;
         self.truncateDisclosure();
     };
 
     RevDisclose.prototype.setOnClickHandler = function (handler, handlerObject) {
-        // console.log("RevDisclose: Setting onClick Handler for Disclosure text...");
         var self = this;
         if (typeof handler === 'function') {
             self.onClickHandler = handler;
@@ -5107,15 +5100,12 @@ return utils;
     };
 
     RevDisclose.prototype.getSponsorTemplate = function () {
-        // console.log("RevDisclose: Building Disclosure HTML...");
         var self = this;
         self.disclosureHtml = '<a href="javascript:;" onclick="revDisclose.onClickHandler(revDisclose.onClickHandlerObject ? revDisclose.onClickHandlerObject : null);">' + self.disclosureText + '</a>';
-        // console.log("RevDisclose: " + self.disclosureHtml);
         return self.plainText ? self.disclosureText : self.disclosureHtml;
     };
 
     RevDisclose.prototype.getDisclosure = function (disclosureText) {
-        // console.log("RevDisclose: Attaching Disclosure Text and onClick Event Function...");
         var self = this;
         self.setDisclosureText(disclosureText);
         if(typeof self.dialog === "object") {
