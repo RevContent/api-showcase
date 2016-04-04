5ce74490ee9537e297d71277f9edc2984ddee273
Michael Osterhout <michael@revcontent.com>
2016-04-04 14:58:50 -0400

    better per_row defaults for mobile considering text is on the right

--- htdocs/showcase/revcontent-api-showcase-shifter-2.0/js/revshifter.js
+++ htdocs/showcase/revcontent-api-showcase-shifter-2.0/js/revshifter.js
@@ -49,9 +49,9 @@ RevShifter({
                 xxs: 1,
                 xs: 1,
                 sm: 2,
-                md: 3,
-                lg: 4,
-                xl: 5,
+                md: 2,
+                lg: 3,
+                xl: 4,
                 xxl: 5
             },
             rows: 1,
