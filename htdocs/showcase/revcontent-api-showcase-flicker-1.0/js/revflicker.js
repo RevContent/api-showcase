bc18d10a6c8e635fcbcf50bf77ba732234be59b6
Michael Osterhout <michael@revcontent.com>
2016-04-11 16:05:03 -0400

    unset impressionTracker on failure

--- htdocs/showcase/revcontent-api-showcase-flicker-1.0/js/revflicker.js
+++ htdocs/showcase/revcontent-api-showcase-flicker-1.0/js/revflicker.js
@@ -441,6 +441,9 @@ RevFlicker({
 
             revApi.request(impressionsUrl, function() {
                 if(offset === 0 && true === that.options.beacons) { revApi.beacons.setPluginSource('flicker').attach(); }
+            }, function() {
+                that.impressionTracker[offset + '_' + count] = false; //unset on failure in case we somehow try it again
+                //TODO: retry the call or log to db for later attempt
             });
         }
     }
