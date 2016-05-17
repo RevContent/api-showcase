b01ac00880cd7995f9fa2ab42ef194df3e3b7c68
Julien Chinapen <julien@revcontent.com>
2016-05-17 15:46:05 -0400

    Build
    Asset rebuild.
    
    Signed-off-by: Julien Chinapen <julien@revcontent.com>

--- htdocs/showcase/revcontent-api-showcase-exitpop-1.0/gulpfile.js
+++ htdocs/showcase/revcontent-api-showcase-exitpop-1.0/gulpfile.js
@@ -84,7 +84,7 @@ gulp.task('build-rx', ['revexit-css', 'revchimp-css', 'revchimp-inject', 'revexi
       ' */',
       ''].join('\n');
 
-    return gulp.src(['../js/revbeacon.js', '../js/revapi.js', '../js/revdialog.js', '../js/jquery-1.11.3.js', './build/revchimp.js', './build/revexit.js'])
+    return gulp.src(['../js/revbeacon.js', '../js/revapi.js', '../js/revdialog.js', './js/jquery-1.11.3.js', './build/revchimp.js', './build/revexit.js'])
         .pipe(concat('revexit.pkgd.js'))
         .pipe(gulp.dest('./build'))
         .pipe(uglify({
