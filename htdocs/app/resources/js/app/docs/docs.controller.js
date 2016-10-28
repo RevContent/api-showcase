0abe190363e8cd3dc2ba244be9caee65c7fcf693
Michael Osterhout <michael@revcontent.com>
2016-10-28 17:44:33 -0400

    improve widget docs list style

--- htdocs/app/resources/js/app/docs/docs.controller.js
+++ htdocs/app/resources/js/app/docs/docs.controller.js
@@ -1,4 +1,5 @@
-app.controller('DocsCtrl', function($stateParams, $mdToast, $rootScope, $scope, $http, $timeout, $mdBottomSheet) {
+app.controller('DocsCtrl', function($stateParams, $mdToast, $rootScope, $scope, $http, $timeout, $mdBottomSheet, widgets) {
+    $scope.widgets = widgets.data;
     $scope.active = {
         request: 'jsonp',
         response: 'json'
