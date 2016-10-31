beb886f9ee1101d172244dc99174543571be8df8
Michael Osterhout <michael@revcontent.com>
2016-10-31 11:40:51 -0400

    delete widgets that don’t have docs

--- htdocs/app/resources/js/app/docs/docs.controller.js
+++ htdocs/app/resources/js/app/docs/docs.controller.js
@@ -1,4 +1,7 @@
 app.controller('DocsCtrl', function($stateParams, $mdToast, $rootScope, $scope, $http, $timeout, $mdBottomSheet, widgets) {
+    delete widgets.data['amp'];
+    delete widgets.data['instream'];
+    delete widgets.data['end-of-gallery'];
     $scope.widgets = widgets.data;
     $scope.active = {
         request: 'jsonp',
