78475addab9a4799506394a7ba4236cff79a1104
Aziz Hussain <aziz@revcontent.com>
2017-06-24 13:08:52 -0400

    Fix code

--- htdocs/showcase/revcontent-api-showcase-in-feed-video-1.0/js/infeedvideo.js
+++ htdocs/showcase/revcontent-api-showcase-in-feed-video-1.0/js/infeedvideo.js
@@ -58,7 +58,7 @@ Author: michael@revcontent.com
         connatixScript.setAttribute('pcp2', this.config.pub_id);
         connatixScript.setAttribute('customlogo', 'https://i.connatix.com/s3/connatix-uploads/assets/RCIcon3.png?mode=pad&height=18');
         if ('video_id' in this.config && this.config.video_id > 0) {
-            connatixScript.setAttribute('cnx-video-id="', this.config.video_id);
+            connatixScript.setAttribute('cnx-video-id', this.config.video_id);
         }
         revUtils.append(this.element, connatixScript);
     }
