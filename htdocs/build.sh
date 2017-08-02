d40be8c6190a2a66f3ec45b711606984f11a4a54
Julien Chinapen <julien@revcontent.com>
2017-08-02 07:50:52 -0400

    Build.sh
    Add utility script for build process.
    
    Signed-off-by: Julien Chinapen <julien@revcontent.com>

---
+++ htdocs/build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+gulp build --widget=slider
+gulp build --widget=slider2
+gulp build --widget=flicker
+gulp build --widget=shifter
+gulp build --widget=scroller
+gulp build --widget=image
+gulp build --widget=lock
+gulp build --widget=more
+gulp build --widget=soloserve
+gulp build --widget=toaster
+gulp build --widget=interstitial
+gulp build --widget=infeedvideo
+gulp build --widget=instream
+gulp build --widget=side-shifter
+
+# Rx Build (Isolated)
+cd showcase/revcontent-api-showcase-exitpop-1.0 && gulp
