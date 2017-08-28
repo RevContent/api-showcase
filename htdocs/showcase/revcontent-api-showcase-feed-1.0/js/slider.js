c26930366f4cd5728db949fe1300205a329e8b8a
Michael Osterhout <michael@revcontent.com>
2017-08-28 11:45:51 -0400

    timeAgo

--- htdocs/showcase/revcontent-api-showcase-feed-1.0/js/slider.js
+++ htdocs/showcase/revcontent-api-showcase-feed-1.0/js/slider.js
@@ -2193,13 +2193,8 @@ Author: michael@revcontent.com
 
                 var date = item.element.querySelector('.rev-date');
                 if (date) {
-                    if (itemData.date) {
-                        date.innerHTML = itemData.date;
-                    } else {
-                        date.innerHTML = (Math.floor(Math.random() * (24 - 1 + 1)) + 1) + ' hrs';
-                    }
+                    date.innerHTML = this.timeAgo(itemData.date);
                 }
-
 
                 var favicon = item.element.querySelector('.rev-headline-icon');
                 if (favicon) {
@@ -2341,6 +2336,57 @@ Author: michael@revcontent.com
         // this.checkEllipsis();
         // this.updatePagination();
         // this.fitHeight();
+    };
+
+    RevSlider.prototype.timeAgo = function(time) {
+        var templates = {
+            prefix: "",
+            suffix: "",
+            seconds: "less than a minute",
+            minute: "about a minute",
+            minutes: "%d minutes",
+            hour: "1 hr",
+            hours: "%d hrs",
+            day: "a day",
+            days: "%d days",
+            month: "1 month",
+            months: "%d months",
+            year: "1 year",
+            years: "%d years"
+        };
+        var template = function(t, n) {
+            return templates[t] && templates[t].replace(/%d/i, Math.abs(Math.round(n)));
+        };
+
+        // random hrs
+        if (!time)
+            return (Math.floor(Math.random() * (24 - 1 + 1)) + 1) + ' hrs';
+        time = time.replace(/\.\d+/, ""); // remove milliseconds
+        time = time.replace(/-/, "/").replace(/-/, "/");
+        time = time.replace(/T/, " ").replace(/Z/, " UTC");
+        time = time.replace(/([\+\-]\d\d)\:?(\d\d)/, " $1$2"); // -04:00 -> -0400
+        time = new Date(time * 1000 || time);
+
+        var now = new Date();
+        var seconds = ((now.getTime() - time) * .001) >> 0;
+        var minutes = seconds / 60;
+        var hours = minutes / 60;
+        var days = hours / 24;
+        var years = days / 365;
+
+        return templates.prefix + (
+                seconds < 45 && template('seconds', seconds) ||
+                seconds < 90 && template('minute', 1) ||
+                minutes < 45 && template('minutes', minutes) ||
+                minutes < 90 && template('hour', 1) ||
+                hours < 24 && template('hours', hours) ||
+                hours < 42 && template('day', 1) ||
+                days < 30 && template('days', days) ||
+                days < 45 && template('month', 1) ||
+                days < 365 && template('months', days / 30) ||
+                years < 1.5 && template('year', 1) ||
+                template('years', years)
+                ) + templates.suffix;
     };
 
     RevSlider.prototype.fitHeight = function() {
