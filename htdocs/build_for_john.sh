8015a05d22b1cfffeb2c0073008738c1813f95fa
Harsh Jain <harsh@crispygam.es>
2017-09-15 17:49:46 +0530

    Build Stuff

--- htdocs/build_for_john.sh
+++ htdocs/build_for_john.sh
@@ -1,2 +1,3 @@
 gulp build --widget=feed2
 cp build/files/feed2/revfeed2.pkgd.js showcase/revcontent-api-showcase-feed-1.0/js/revfeed2.pkgd.js
+cp build/revfeed2.min.js showcase/revcontent-api-showcase-feed-1.0/js/revfeed2.min.js
