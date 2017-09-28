b339e58d92e51d1f52ed16f40428deb669451323
Michael Osterhout <michael@revcontent.com>
2017-09-28 13:44:56 -0400

    always register imps

--- htdocs/showcase/revcontent-api-showcase-feed-1.0/js/slider.js
+++ htdocs/showcase/revcontent-api-showcase-feed-1.0/js/slider.js
@@ -2273,8 +2273,8 @@ Author: michael@revcontent.com
             // if the first internal is greater than 0 there is a ba
             var firstInternal = revUtils.siblingIndex(this.grid.element.querySelector(this.options.internal_selector));
             // don't register multiple widget impressions
-            var fill = urls.length > 0;
-            var sponsoredURL = this.generateUrl(0, this.sponsoredLimit, false, false, false, (firstInternal > 0), fill);
+            // var fill = urls.length > 0;
+            var sponsoredURL = this.generateUrl(0, this.sponsoredLimit, false, false, false, (firstInternal > 0));
             urls.push({
                 offset: 0,
                 limit: this.sponsoredLimit,
