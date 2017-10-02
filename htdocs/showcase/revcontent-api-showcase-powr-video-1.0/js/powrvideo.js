54121630deafa8b8c0feb25927817608af1e3e13
Harsh Jain <harsh@revcontent.com>
2017-10-02 18:25:18 +0530

    Float Issues Fixed

--- htdocs/showcase/revcontent-api-showcase-powr-video-1.0/js/powrvideo.js
+++ htdocs/showcase/revcontent-api-showcase-powr-video-1.0/js/powrvideo.js
@@ -4,6 +4,19 @@
  Author: harsh@revcontent.com
  */
 
+
+if (!String.prototype.startsWith) {
+  String.prototype.startsWith = function(searchString, position) {
+    position = position || 0;
+    return this.indexOf(searchString, position) === position;
+  };
+}
+
+if (!String.prototype.endsWith) {
+    String.prototype.endsWith = function(suffix) {
+	return this.indexOf(suffix, this.length - suffix.length) !== -1;
+    };
+}
 // universal module definition
 ( function( window, factory ) {
     // browser global
