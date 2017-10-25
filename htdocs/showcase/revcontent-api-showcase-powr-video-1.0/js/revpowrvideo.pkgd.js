27e06e766b90f8d2586e2e144eb4d4e48c8e14b8
Harsh Jain <harsh@revcontent.com>
2017-10-26 01:37:36 +0530

    Fixes

--- htdocs/showcase/revcontent-api-showcase-powr-video-1.0/js/revpowrvideo.pkgd.js
+++ htdocs/showcase/revcontent-api-showcase-powr-video-1.0/js/revpowrvideo.pkgd.js
@@ -26138,6 +26138,7 @@ if (!String.prototype.endsWith) {
 	if (this.orientation == "landscape" && !this.floatSettings.landscape)
 	    return;
 
+	utils.addClass(document.body, 'powr_player_floating');
         this.container.className = "rc-float-video powr_player " + (this.permanentClose == "yes" ? "powr_permanent_close" : "");
         var styleString = "";
         var fs = this.floatSettings;
@@ -26239,6 +26240,7 @@ if (!String.prototype.endsWith) {
 
     PowrVideo.prototype.unfloatPlayer = function() {
         if (this.floated) {
+	    utils.removeClass(document.body, 'powr_player_floating');
             this.container.className = 'powr_player ' + (this.permanentClose == "yes" ? 'powr_permanent_close' : '');
             this.container.removeAttribute("style");
             this.floated = false;
