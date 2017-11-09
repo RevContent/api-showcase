c68e399f0323fb7ede50e5342f4ecd74d6a32815
Harsh Jain <harsh@revcontent.com>
2017-11-10 01:32:14 +0530

    Fix Embed Issue and sites overriding our pages

--- htdocs/build_for_john.sh
+++ htdocs/build_for_john.sh
@@ -1,4 +1,8 @@
 gulp build --widget=powrvideo
+cp showcase/revcontent-api-showcase-powr-video-1.0/js/powriframefix.js build/powriframefix.js
+
 cp build/files/powrvideo/revpowrvideo.pkgd.js showcase/revcontent-api-showcase-powr-video-1.0/js/revpowrvideo.pkgd.js
+cp build/files/powrutils/revpowrutils.pkgd.js showcase/revcontent-api-showcase-powr-video-1.0/js/revpowrutils.pkgd.js
 cp build/revpowrvideo.min.js showcase/revcontent-api-showcase-powr-video-1.0/js/revpowrvideo.min.js
+cp build/revpowrutils.min.js showcase/revcontent-api-showcase-powr-video-1.0/js/revpowrutils.min.js
 rsync -rtv -e ssh ./showcase/revcontent-api-showcase-powr-video-1.0/* $1@code.revcontent.com:/srv/www/revcontent.com/code/htdocs/mockrc11/powr3
