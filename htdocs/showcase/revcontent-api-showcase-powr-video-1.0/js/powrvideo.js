36fcf55662c61fc8206c26cf0f178ab3edc9633d
Deepak Kumar Yadav <deepak@revcontent.com>
2018-01-09 20:21:32 +0530

    minor

--- htdocs/showcase/revcontent-api-showcase-powr-video-1.0/js/powrvideo.js
+++ htdocs/showcase/revcontent-api-showcase-powr-video-1.0/js/powrvideo.js
@@ -404,7 +404,7 @@ if (!String.prototype.endsWithPowr) {
         }
 
         var contentSrc = document.createElement('source');
-        if(this.mobile && this.videos[0].mobile_url != null) {
+        if(this.mobile && (this.videos[0].mobile_url != null)) {
           contentSrc.setAttribute('src', this.videos[0].mobile_url);
         } else {
           contentSrc.setAttribute('src', this.videos[0].sd_url);
@@ -536,7 +536,7 @@ if (!String.prototype.endsWithPowr) {
       this.player.ima(this.options, this.bind(this, this.adsManagerLoadedCallback));
       this.player.ima.initializeAdDisplayContainer();
       // this.player.ima.setContentWithAdTag(this.videos[this.currentContent].sd_url, this.getAdTag(this.videos[this.currentContent].id), playOnLoad);
-      if(this.mobile && this.videos[this.currentContent].mobile_url != null) {
+      if(this.mobile && (this.videos[this.currentContent].mobile_url != null)) {
         this.player.ima.setContentWithAdsResponse(this.videos[this.currentContent].mobile_url, this.getAdsResponse(this.videos[this.currentContent]), playOnLoad);
       } else {
         this.player.ima.setContentWithAdsResponse(this.videos[this.currentContent].sd_url, this.getAdsResponse(this.videos[this.currentContent]), playOnLoad);
@@ -579,7 +579,7 @@ if (!String.prototype.endsWithPowr) {
 	    this.adsPlayed = 0;
 
 	    this.player.ima.initializeAdDisplayContainer();
-      if(this.mobile && this.videos[this.currentContent].mobile_url != null) {
+      if(this.mobile && (this.videos[this.currentContent].mobile_url != null)) {
         this.player.ima.setContentWithAdsResponse(this.videos[this.currentContent].mobile_url, this.getAdsResponse(this.videos[this.currentContent]), false);
       } else {
         this.player.ima.setContentWithAdsResponse(this.videos[this.currentContent].sd_url, this.getAdsResponse(this.videos[this.currentContent]), false);
