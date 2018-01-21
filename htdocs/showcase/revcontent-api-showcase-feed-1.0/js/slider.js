3161179a08f1000d22e6930818c736a12a2f8da0
Harsh Jain <harsh@revcontent.com>
2018-01-21 18:08:50 -0500

    Fix Time

--- htdocs/showcase/revcontent-api-showcase-feed-1.0/js/slider.js
+++ htdocs/showcase/revcontent-api-showcase-feed-1.0/js/slider.js
@@ -1043,7 +1043,7 @@ Author: michael@revcontent.com
             revImage.style.backgroundImage = 'url('+ image +')';
             // revImage.innerHTML = '<img src=" ' + image + ' " />';
         } else {
-	    var cb = new Date().getMilliseconds();
+	    var cb = new Date().getTime();
             var site_url = document.location.href;
             revImage.innerHTML = '<iframe id="rc_video' + item.data.video_id + '" src="//video.powr.com/video.js.php?if=true&v=' + item.data.video_id + '&uid='+this.options.pub_id+'&t=1&c='+cb+'&su='+ encodeURI(site_url) +'&adt=-1" style="border: none; width: '+ roundedPreloaderWidth +'px; height: ' + roundedPreloaderHeight + 'px;""></iframe>';
             // revImage.innerHTML = '<iframe id="rc_video' + item.data.video_id + '" src="http://code.revcontent.com/mock/feed4/video' + item.data.video_id + '.iframe.html" style="border: none; width: '+ roundedPreloaderWidth +'px; height: ' + roundedPreloaderHeight + 'px;""></iframe>';
