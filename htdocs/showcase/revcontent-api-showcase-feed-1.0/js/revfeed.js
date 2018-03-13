f78ec0cf2749559c4faff2cfe7f9166684ca8ea1
Chad Ladensack <chad@revcontent.com>
2018-03-13 11:38:13 -0400

    Moved the SVG loader to base64 encoded within the style sheet.

--- htdocs/showcase/revcontent-api-showcase-feed-1.0/js/revfeed.js
+++ htdocs/showcase/revcontent-api-showcase-feed-1.0/js/revfeed.js
@@ -737,65 +737,12 @@ Author: michael@revcontent.com
         self.loadMoreText.className = 'rev-loadmore-button-text';
         self.loadMoreText.innerHTML = 'LOAD MORE CONTENT';
 
-        self.loadMoreSpinner = document.createElement('div');
-        self.loadMoreSpinner.setAttribute('style', 'display:none');
-        self.loadMoreSpinner.innerHTML = '<svg class="lds-spinner" width="30px"  height="30px"  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" style="background: none;"><g transform="rotate(0 50 50)">\n' +
-            '  <rect x="47" y="24" rx="9.4" ry="4.8" width="6" height="12" fill="#ffffff">\n' +
-            '    <animate attributeName="opacity" values="1;0" times="0;1" dur="1s" begin="-0.9166666666666666s" repeatCount="indefinite"></animate>\n' +
-            '  </rect>\n' +
-            '</g><g transform="rotate(30 50 50)">\n' +
-            '  <rect x="47" y="24" rx="9.4" ry="4.8" width="6" height="12" fill="#ffffff">\n' +
-            '    <animate attributeName="opacity" values="1;0" times="0;1" dur="1s" begin="-0.8333333333333334s" repeatCount="indefinite"></animate>\n' +
-            '  </rect>\n' +
-            '</g><g transform="rotate(60 50 50)">\n' +
-            '  <rect x="47" y="24" rx="9.4" ry="4.8" width="6" height="12" fill="#ffffff">\n' +
-            '    <animate attributeName="opacity" values="1;0" times="0;1" dur="1s" begin="-0.75s" repeatCount="indefinite"></animate>\n' +
-            '  </rect>\n' +
-            '</g><g transform="rotate(90 50 50)">\n' +
-            '  <rect x="47" y="24" rx="9.4" ry="4.8" width="6" height="12" fill="#ffffff">\n' +
-            '    <animate attributeName="opacity" values="1;0" times="0;1" dur="1s" begin="-0.6666666666666666s" repeatCount="indefinite"></animate>\n' +
-            '  </rect>\n' +
-            '</g><g transform="rotate(120 50 50)">\n' +
-            '  <rect x="47" y="24" rx="9.4" ry="4.8" width="6" height="12" fill="#ffffff">\n' +
-            '    <animate attributeName="opacity" values="1;0" times="0;1" dur="1s" begin="-0.5833333333333334s" repeatCount="indefinite"></animate>\n' +
-            '  </rect>\n' +
-            '</g><g transform="rotate(150 50 50)">\n' +
-            '  <rect x="47" y="24" rx="9.4" ry="4.8" width="6" height="12" fill="#ffffff">\n' +
-            '    <animate attributeName="opacity" values="1;0" times="0;1" dur="1s" begin="-0.5s" repeatCount="indefinite"></animate>\n' +
-            '  </rect>\n' +
-            '</g><g transform="rotate(180 50 50)">\n' +
-            '  <rect x="47" y="24" rx="9.4" ry="4.8" width="6" height="12" fill="#ffffff">\n' +
-            '    <animate attributeName="opacity" values="1;0" times="0;1" dur="1s" begin="-0.4166666666666667s" repeatCount="indefinite"></animate>\n' +
-            '  </rect>\n' +
-            '</g><g transform="rotate(210 50 50)">\n' +
-            '  <rect x="47" y="24" rx="9.4" ry="4.8" width="6" height="12" fill="#ffffff">\n' +
-            '    <animate attributeName="opacity" values="1;0" times="0;1" dur="1s" begin="-0.3333333333333333s" repeatCount="indefinite"></animate>\n' +
-            '  </rect>\n' +
-            '</g><g transform="rotate(240 50 50)">\n' +
-            '  <rect x="47" y="24" rx="9.4" ry="4.8" width="6" height="12" fill="#ffffff">\n' +
-            '    <animate attributeName="opacity" values="1;0" times="0;1" dur="1s" begin="-0.25s" repeatCount="indefinite"></animate>\n' +
-            '  </rect>\n' +
-            '</g><g transform="rotate(270 50 50)">\n' +
-            '  <rect x="47" y="24" rx="9.4" ry="4.8" width="6" height="12" fill="#ffffff">\n' +
-            '    <animate attributeName="opacity" values="1;0" times="0;1" dur="1s" begin="-0.16666666666666666s" repeatCount="indefinite"></animate>\n' +
-            '  </rect>\n' +
-            '</g><g transform="rotate(300 50 50)">\n' +
-            '  <rect x="47" y="24" rx="9.4" ry="4.8" width="6" height="12" fill="#ffffff">\n' +
-            '    <animate attributeName="opacity" values="1;0" times="0;1" dur="1s" begin="-0.08333333333333333s" repeatCount="indefinite"></animate>\n' +
-            '  </rect>\n' +
-            '</g><g transform="rotate(330 50 50)">\n' +
-            '  <rect x="47" y="24" rx="9.4" ry="4.8" width="6" height="12" fill="#ffffff">\n' +
-            '    <animate attributeName="opacity" values="1;0" times="0;1" dur="1s" begin="0s" repeatCount="indefinite"></animate>\n' +
-            '  </rect>\n' +
-            '</g></svg>';
-
-        revUtils.append(self.loadMoreContainer, self.loadMoreSpinner);
         revUtils.append(self.loadMoreContainer, self.loadMoreText);
         revUtils.append(self.innerWidget.containerElement, self.loadMoreContainer);
 
         self.loadMoreListener = function() {
             self.loadMoreText.innerHTML = 'LOADING ...';
-            self.loadMoreSpinner.setAttribute('style', 'display:inline');
+            self.loadMoreContainer.className = 'rev-loadmore-button loading';
             revUtils.removeEventListener(self.loadMoreContainer, 'click', self.loadMoreListener);
 
             var beforeItemCount = self.innerWidget.grid.items.length;
@@ -806,7 +753,7 @@ Author: michael@revcontent.com
                 .then(self.promiseUpdateCardData)
                 .then(function(input) {
                     self.loadMoreText.innerHTML = 'LOAD MORE CONTENT';
-                    self.loadMoreSpinner.setAttribute('style', 'display:none');
+                    self.loadMoreContainer.className = 'rev-loadmore-button';
                     revUtils.addEventListener(self.loadMoreContainer, 'click', self.loadMoreListener);
 
                     return input;
