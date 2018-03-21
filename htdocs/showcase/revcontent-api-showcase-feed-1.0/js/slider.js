4b9ec01a132e7fd3729c106bf410d2ca1deb842a
Randy Rist <rrist@ip-10-120-50-56.ec2.internal>
2018-03-21 19:19:12 -0400

    show comment box on click, update avatar newsweek

--- htdocs/showcase/revcontent-api-showcase-feed-1.0/js/slider.js
+++ htdocs/showcase/revcontent-api-showcase-feed-1.0/js/slider.js
@@ -2067,6 +2067,7 @@ Author: michael@revcontent.com
         if (item.type !== 'sponsored' && this.options.comments_enabled) {
 
             var commentBoxElement = this.createCommentInput("comment");
+            commentBoxElement.style.display = 'none';
             var submitCommentBtn = commentBoxElement.querySelector('.comment-submit-button');
             var commentTextAreaElement = commentBoxElement.querySelector('.comment-textarea');
 
@@ -2079,7 +2080,9 @@ Author: michael@revcontent.com
                 commentButton.itemData = itemData;
                 revUtils.addEventListener(commentButton, 'click', function(ev) {
                     that.handleComments(this.itemData, false);
-                    //commentTextAreaElement.focus();
+                    commentBoxElement.style.display = 'block';
+                    that.grid.layout();
+                    commentTextAreaElement.focus();
                 });
             }
 
@@ -3951,7 +3954,8 @@ Author: michael@revcontent.com
 
                 if (mode === "register") {
                     var engage_auth_register = document.createElement('a');
-                    engage_auth_register.innerText = 'REGISTER';
+                    revUtils.addClass(engage_auth_register, 'engage-auth-register-button');
+                    engage_auth_register.innerHTML = '<svg aria-hidden="true" data-prefix="fas" data-icon="share" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-share fa-w-16 fa-7x"><path fill="currentColor" d="M503.691 189.836L327.687 37.851C312.281 24.546 288 35.347 288 56.015v80.053C127.371 137.907 0 170.1 0 322.326c0 61.441 39.581 122.309 83.333 154.132 13.653 9.931 33.111-2.533 28.077-18.631C66.066 312.814 132.917 274.316 288 272.085V360c0 20.7 24.3 31.453 39.687 18.164l176.004-152c11.071-9.562 11.086-26.753 0-36.328z" class=""></path></svg>';
                     engage_auth_box_inner.appendChild(engage_auth_register);
                 } else {
                     var engage_auth_login = document.createElement('a');
@@ -4028,8 +4032,8 @@ Author: michael@revcontent.com
                             return false;
                         }
 
-                        if (engage_auth_password_input.value.length === 0) {
-                            //valid email
+                        if (engage_auth_password_input.value.length !== 0) {
+                            //valid password
                         } else {
                             engage_auth_password_input.focus();
                             engage_auth_password_input.innerText = 'Please enter a password';
