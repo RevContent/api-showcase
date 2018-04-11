b1497382c9af48f6cb0c9f50429b786a8eecb958
Randy Rist <rrist@ip-10-120-50-56.ec2.internal>
2018-04-11 12:50:20 -0400

    ENG-429

--- htdocs/showcase/revcontent-api-showcase-feed-1.0/js/slider.js
+++ htdocs/showcase/revcontent-api-showcase-feed-1.0/js/slider.js
@@ -4753,7 +4753,7 @@ Author: michael@revcontent.com
                         } else {
                             engage_auth_email_input.focus();
                             engage_auth_email_input_error_text.innerText = 'Please enter a valid email';
-                            //return false;
+                            return false;
                         }
 
                         if (engage_auth_password_input.value.length !== 0) {
@@ -4761,7 +4761,7 @@ Author: michael@revcontent.com
                         } else {
                             engage_auth_password_input.focus();
                             engage_auth_password_input.innerText = 'Please enter a password';
-                            //return false;
+                            return false;
                         }
 
                         //get values
@@ -4925,7 +4925,13 @@ Author: michael@revcontent.com
 
                             }
 
-                        },null,false,options);
+                        },function(data){
+                            if (data.status === 500) {
+                                //bad user
+                                engage_auth_email_input.focus();
+                                engage_auth_email_input_error_text.innerText = 'User already exists';
+                            }
+                        },false,options);
                     };
 
                     revUtils.addEventListener(engage_auth_register, 'click', function(){
