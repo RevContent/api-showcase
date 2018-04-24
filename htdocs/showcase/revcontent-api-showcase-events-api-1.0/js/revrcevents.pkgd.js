225ab372e8d2eddff382f4abab4003eb2e6737f7
Mikhail <mikhail@revcontent.com>
2018-04-24 15:29:33 -0700

    js conversions / events api

--- htdocs/showcase/revcontent-api-showcase-events-api-1.0/js/revrcevents.pkgd.js
+++ htdocs/showcase/revcontent-api-showcase-events-api-1.0/js/revrcevents.pkgd.js
@@ -756,21 +756,55 @@ return api;
 
 }));
 /*
- Project: PowrVideo
+ Project: Events
  Version: 1
- Author: harsh@revcontent.com
+ Author: mikhail@revcontent.com
  */
 // universal module definition
 
-var API_KEY = null;
+( function( window, factory) {
+	/*global define: false, module: false, require: false */
+	'use strict';
+	window.revEvents = factory(
+		window,
+		window.revApi
+	);
 
-(function (window) {
-  window.rceInit = function(apiKey) {
-    API_KEY = apiKey;
-  }
+	window.rceInit = function(userId) {
+		return window.revEvents.init(userId);
+	}
 
-  window.rceTrack = function(eventName, props) {
-    alert("Tracking event " + eventName + " with api key " + API_KEY);
-  }
+	window.rceTrack = function(eventName, props) {
+		return window.revEvents.track(eventName, props);
+	}
 
-}( window ));
+}( window, function factory( window, revApi ) {
+
+	'use strict';
+
+	var events = {
+		ENDPOINT : "//trends.revcontent.com/api/v1/events/track.php",
+		USER_ID : ""
+	};
+
+	events.init = function(userId) {
+		this.USER_ID = userId;
+		return this;
+	}
+
+	events.track = function(eventName, props) {
+		revApi.request(
+			this.ENDPOINT + "?u=" + encodeURIComponent(this.USER_ID)
+						  + "&e=" + encodeURIComponent(eventName)
+						  + "&p=" + encodeURIComponent(btoa(JSON.stringify(props))),
+			function(r) {
+				// console.log("success " + r);
+			},
+			function(r) {
+				// console.log("fail " + r)
+			}
+		);
+	}
+
+	return events;
+}));
