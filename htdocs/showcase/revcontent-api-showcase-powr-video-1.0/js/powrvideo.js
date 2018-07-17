2f545413ededc29c2f6bae176684dd64bd574d0e
Peter Wilson-Ferrer <peter@revcontent.com>
2018-07-16 23:15:57 -0400

    testing more destruction

--- htdocs/showcase/revcontent-api-showcase-powr-video-1.0/js/powrvideo.js
+++ htdocs/showcase/revcontent-api-showcase-powr-video-1.0/js/powrvideo.js
@@ -237,7 +237,8 @@ if (!String.prototype.endsWithPowr) {
             width = parseInt(this.getPlayerWidth());
             height = parseInt(this.getPlayerHeight());
         }
-
+this.config.adserver = "ss";
+        console.log(this.config.adserver);
         if (this.config.adserver == "dfp") {
             var ret = "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=" + tag + "&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1"
                 + "&cust_params=p_width%3D" + width + "%26p_height%3D" + height
@@ -251,10 +252,9 @@ if (!String.prototype.endsWithPowr) {
             var ret = "//v.lkqd.net/ad?pid=456&sid=" + tag + "&output=vastvpaid&support=html5flash&execution=any&placement=&playinit=auto&volume=100&width=" + width + "&height=" + height + "&dnt=0&pageurl=" + url + "&contentid=" + video.id + "&contenttitle=" + encodeURI(video.title) + "&contentlength=" + video.duration + "&contenturl=" + encodeURI("http://www.powr.com/video/" + video.id) + "&rnd=" + new Date().getTime();
             return ret;
         } else if (this.config.adserver == "ss") {
-	        var ret = "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dlinearvpaid2js&correlator=";
-            var syndicationid = (this.config.syndication_id && this.config.syndication_id != '') ? "&syid=" + this.config.syndication_id : "";
+	        var syndicationid = (this.config.syndication_id && this.config.syndication_id != '') ? "&syid=" + this.config.syndication_id : "";
             var subid = (this.config.subid && this.config.subid != '') ? "&suid=" + this.config.subid : "";
-        //    var ret = "//vid.springserve.com/vast/" + tag + "?w=" + width + "&h=" + height + "&url=" + url + "&cb=" + new Date().getTime() + syndicationid + subid;
+            var ret = "//vid.springserve.com/vast/" + tag + "?w=" + width + "&h=" + height + "&url=" + url + "&cb=" + new Date().getTime() + syndicationid + subid;
             return ret;
         } else {
             tag = tag.replace("REFERRER_URL", url);
@@ -1543,7 +1543,7 @@ if (!String.prototype.endsWithPowr) {
             }
         })(document.createElement('div'));
 
-        this.element.classList.add("animated", "zoomOut");
+        this.element.parentNode.classList.add("animated", "zoomOut");
         this.element.addEventListener(animationEnd, function(e) {
             e.target.removeEventListener(e.type, arguments.callee);
             elementId.setAttribute("style", "width: 0px; height : 0px; position : relative;");
