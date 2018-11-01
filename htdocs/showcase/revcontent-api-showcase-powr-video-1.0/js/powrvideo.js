18949c1fa101fad3f691bccf04d8e3f511563c22
Peter Wilson-Ferrer <peter@revcontent.com>
2018-11-01 01:05:37 -0400

    more tbo phun

--- htdocs/showcase/revcontent-api-showcase-powr-video-1.0/js/powrvideo.js
+++ htdocs/showcase/revcontent-api-showcase-powr-video-1.0/js/powrvideo.js
@@ -131,6 +131,11 @@ if (!String.prototype.endsWithPowr) {
             hs = parseInt(h) + "px";
         }
 
+        if (window.location.href.indexOf("powrtest=1") > 0) {
+            parent.document.getElementById(frameElement.id).width = w;
+            parent.document.getElementById(frameElement.id).height = h;
+        }
+
         this.videos = config.videos;
 
         if (this.videos.length == 0) {
@@ -254,18 +259,7 @@ if (!String.prototype.endsWithPowr) {
                 width = 640;
                 if (window.location.href.indexOf("powrtest=1") > 0) {
                     this.animateShow();
-                   // var sfAPI = window.sfAPI || $sf.ext;
-                   // var self = sfAPI.geom().self;
-                   // var el = document.getElementById('container');
-                   // el.style.width = '640px';
-                   // el.style.height = '368px';
                     tag = 849943;
-
-                   // sfAPI.register(500, 500, function(status, data) {
-                   //     if (status === 'geom-update') {
-                   //         return;
-                   //     }
-                   // });
                 }
             } else {
                 width = parseInt(this.config.width);
