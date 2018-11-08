ad8790b56cde08969086c3101be2f534671f9492
Peter Wilson-Ferrer <peter@revcontent.com>
2018-11-08 01:40:00 -0500

    :headdesk: what the hell is wrong with VAST tonight

--- htdocs/showcase/revcontent-api-showcase-powr-video-1.0/js/powrvideo.js
+++ htdocs/showcase/revcontent-api-showcase-powr-video-1.0/js/powrvideo.js
@@ -260,6 +260,8 @@ if (!String.prototype.endsWithPowr) {
                 width = 640;
             } else if (this.config.pub_id == 98997) {
                 // width = parseInt(this.getPlayerWidth());
+                tag = "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dskippablelinear&correlator=";
+                adserver = "custom";
                 width = 640;
             } else {
                 width = parseInt(this.config.width);
