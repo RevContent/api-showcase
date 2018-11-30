16ca1a76530770e37985a29b4db9957029a57236
Peter Wilson-Ferrer <peter@revcontent.com>
2018-11-30 00:50:35 -0500

    Peter needs sleep. Space between classes

--- htdocs/showcase/revcontent-api-showcase-powr-video-1.0/js/powrvideo.js
+++ htdocs/showcase/revcontent-api-showcase-powr-video-1.0/js/powrvideo.js
@@ -539,9 +539,9 @@ if (!String.prototype.endsWithPowr) {
             this.brandingContainer = document.createElement("div");
             this.brandingContainer.className = "powr_branding";
             if (this.letterbox !== true) {
-                this.brandingContainer.className += "powr_branding_overlay";
+                this.brandingContainer.className += " powr_branding_overlay";
             }
-            this.brandingContainer.innerHTML = '<a target="_blank" href="https://powr.com/getpowr" class="powr_branding_link">Powered by Powr</a>';
+            this.brandingContainer.innerHTML = '<a target="_blank" href="https://powr.com/getpowr" class="powr_branding_text">Powered by Powr</a>';
             this.container.appendChild(this.brandingContainer);
         }
 
