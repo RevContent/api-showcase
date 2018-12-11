26c2b612a1339ccbb82c7e49bddb6862fe949238
Peter Wilson-Ferrer <peter@revcontent.com>
2018-12-11 01:37:24 -0500

    testing VAST fallback

--- htdocs/showcase/revcontent-api-showcase-powr-video-1.0/js/powrvideo.js
+++ htdocs/showcase/revcontent-api-showcase-powr-video-1.0/js/powrvideo.js
@@ -269,7 +269,29 @@ if (!String.prototype.endsWithPowr) {
         ret = ret + '<![CDATA[' + "\n";
         ret = ret + url;
         ret = ret + "\n]]>\n";
-        ret = ret + '</vmap:AdTagURI></vmap:AdSource></vmap:AdBreak>';
+
+        if (window.location.href.indexOf("powrfallback=1") > 0) {
+            ret = ret + '</vmap:AdTagURI></vmap:AdSource>'
+                + '<Extensions>'
+                + '<Extension type="waterfall" fallback_index="0"/>' +
+                + '</Extensions>'
+                + '</vmap:AdBreak>';
+                + '<vmap:AdBreak timeOffset="start" breakType="linear" breakId="preroll"><vmap:AdSource id="preroll-ad-1" allowMultipleAds="false" followRedirects="true"><vmap:AdTagURI templateType="vast3">'
+                + "\n"
+                + '<![CDATA[' + "\n"
+                + "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dlinear&correlator="
+                + "\n]]>\n"
+                + '</vmap:AdTagURI>' +
+                + '</vmap:AdSource>'
+                + '<Extensions>'
+                + '<Extension type="waterfall" fallback_index="0"/>' +
+                + '</Extensions>'
+                + '</vmap:AdBreak>';
+
+        } else {
+            ret = ret + '</vmap:AdTagURI></vmap:AdSource></vmap:AdBreak>';
+        }
+
         return ret;
     };
 
